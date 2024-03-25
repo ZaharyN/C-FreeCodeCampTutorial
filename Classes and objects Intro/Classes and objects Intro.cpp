c7de992b406f1c1d7e26fa9310275731a379271a
ZaharyN <zlnyagolov@abv.bg>
2024-03-25 15:04:56 +0200

    Introduction to classes

---
+++ Classes and objects Intro/Classes and objects Intro.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Book 
+{
+    public:
+        string title;
+        string author;
+        int pages;
+};
+
+int main()
+{
+    Book firstBook;
+
+    firstBook.title = "Dune";
+    firstBook.author = "Frank Herbert";
+    firstBook.pages = 560;
+
+    cout << firstBook.author << endl;
+    cout << firstBook.title << endl;
+    cout << firstBook.pages << endl;
+
+    Book secondBook;
+
+    secondBook.title = "IT";
+    secondBook.author = "Stephen King";
+    secondBook.pages = 700;
+
+    string secondBookName = secondBook.title;
+    bool areEqual = secondBookName == secondBook.title;
+    cout << areEqual << endl;
+
+    cout << secondBook.author << endl;
+    cout << secondBook.title << endl;
+    cout << secondBook.pages << endl;
+}
+
--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -21,4 +21,5 @@ int main()
 
     cout << pYears << endl;
    
+    return 0;
 }
