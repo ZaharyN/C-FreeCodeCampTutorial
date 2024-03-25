7db41f6c6b0e797dc050ce6cf8fad19b19e670cc
ZaharyN <zlnyagolov@abv.bg>
2024-03-25 14:24:36 +0200

    Pointers

---
+++ Pointers/Pointers.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 7;
+    int *pAge = &age;
+
+    double years = 25;
+    double *pYears = &years;
+
+    string name = "Zahary";
+    string *pName = &name;
+   
+    cout << pAge << endl;
+
+    cout << *pAge << endl;
+
+    cout << &age << endl;
+
+    cout << pYears << endl;
+   
+}
