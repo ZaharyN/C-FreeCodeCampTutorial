ad6e0cef11c54b36f9cf62978cd372ddbe6a4dc2
ZaharyN <zlnyagolov@abv.bg>
2024-03-25 15:32:53 +0200

    Constructors Intro

---
+++ Constructors Intro/Constructors Intro.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Dog
+{
+    public:
+        string breed;
+        string name;
+        int age;
+
+    Dog(string _breed, string _name, int _age)
+    {
+        breed = _breed;
+        name = _name;
+        age = _age;
+    }
+    Dog()
+    {
+        breed = "street fabulousus";
+        name = "Bau-Bau";
+        age = 10;
+    }
+};
+
+
+
+int main()
+{
+    Dog dogWithOwner("German shepfard", "Joro", 4);
+
+    cout << dogWithOwner.breed << endl;
+    cout << dogWithOwner.name << endl;
+    cout << dogWithOwner.age << endl;
+
+    Dog streetDog;
+
+    cout << streetDog.breed << endl;
+    cout << streetDog.name << endl;
+    cout << streetDog.age;
+}
