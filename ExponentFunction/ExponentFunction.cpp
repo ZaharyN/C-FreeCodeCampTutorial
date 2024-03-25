668eac7f160bd6012210beebaa6845c8e9e154d8
ZaharyN <zlnyagolov@abv.bg>
2024-03-25 10:19:54 +0200

    Added method calculating a given number raised to a given power. Both number and power are given by the user.

---
+++ ExponentFunction/ExponentFunction.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int power(int number, int power)
+{
+    int result = 1;
+
+	for (int i = 0; i < power; i++)
+	{
+		result *= number;
+	}
+
+	return result;
+}
+
+
+int main()
+{
+	int number;
+	int powerNumber;
+
+	cout << "Enter number here: ";
+	cin >> number;
+
+	cout << "Enter power here: ";
+	cin >> powerNumber;
+
+	cout << "The result: ";
+	cout << power(number, powerNumber);
+}
