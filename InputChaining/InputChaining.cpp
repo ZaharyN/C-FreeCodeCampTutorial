b209d5fc83e3c3140d4f17372d1ec91711eb7d5b
ZaharyN <zlnyagolov@abv.bg>
2024-03-27 11:06:57 +0200

    Input chaining

---
+++ InputChaining/InputChaining.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    string city;
+
+    cin >> name >> age >> city;
+
+    cout << "My name is: " << name << ", I am " << age << " years old and I work and live in " << city;
+}
+
+
