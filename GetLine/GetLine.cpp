a2eb5c8a26896201f56d259e79021f45ec87a011
ZaharyN <zlnyagolov@abv.bg>
2024-04-03 16:29:07 +0300

    getline function

---
+++ GetLine/GetLine.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    
+    cout << "Enter string with spaces: ";
+    getline(cin,s);
+
+    cout << s;
+}
+
+
