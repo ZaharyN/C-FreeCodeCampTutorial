64fa9474231298e788dfafa55365c0b6f368a8bf
ZaharyN <zlnyagolov@abv.bg>
2024-04-03 16:29:39 +0300

    pass by reference tested

--- TestInterviewQuestions/TestInterviewQuestions.cpp
+++ TestInterviewQuestions/TestInterviewQuestions.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+void duplicate(int a, int b, int c) {
+	a *= 2;
+	b *= 2;
+	c *= 2;
+}
+
+int main(int argc, char** argv)
 {
-    int i = 5;
-    int j = ++i;
 
-    cout << "i has value: " <<i << endl;
-    cout << "j has value: " << j << endl;
+	int x = 2;
+	int y = 4;
+	int z = 5;
 
-    j = i;
+	duplicate(x, y, z);
 
-    cout << "j has value: " << j << endl;
+	cout << x << y << z;
+
+	return 0;
 }
 
 
