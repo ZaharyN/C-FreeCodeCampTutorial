3417c7c4dbbae2bfefe0bb5e2b48848f0bae5117
ZaharyN <zlnyagolov@abv.bg>
2024-04-05 17:24:13 +0300

    Tested runtime polymorphism in C++ and C#

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,38 @@
 ﻿
 
+Animal animal = new Animal("grey");
+Animal dog = new Dog("grey");
+
+animal.ProduceSound();
+dog.ProduceSound();
+class Animal
+{
+    public Animal(string color)
+    {
+        Color = color;
+
+    }
+    public virtual void ProduceSound()
+    {
+        Console.WriteLine("AAAAAA");
+    }
+    public string Color { get; set; }
+}
+
+class Dog : Animal
+{
+    private string color = "grey";
+    public Dog(string color) 
+        : base(color)
+    {
+    }
+
+    public override void ProduceSound()
+    {
+        Console.WriteLine("Woof woof");
+    }
+}
+
+
+
 
